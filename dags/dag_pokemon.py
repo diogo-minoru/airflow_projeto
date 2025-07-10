dca231e21904768ca5d6cbc708c3b21aae926046
diogominoru <diogominoru@hotmail.com>
2025-07-10 14:13:15 -0300

    adicionando dag pokemon no airflow

---
+++ dags/dag_pokemon.py
@@ -0,0 +1,17 @@
+from include.pokemon_api import RequestPokemon
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    default_args={"owner": "Astro", "retries": 3},
+    tags=["pokemon"],
+)
+
+def dag_pokemon():
+    capturar_pokemon = RequestPokemon()
+    capturar_pokemon.main()
+
+dag_pokemon()
